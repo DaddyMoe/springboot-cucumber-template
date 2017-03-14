55274693a7a2e1f59964c7936deb6d0724001a11
Mansaray, Moses (UK) <moses.mansaray@euromoneyplc.com>
2017-03-14 11:06:54 +0000

    Initial commit of template Spring boot Hello World

---
+++ src/main/java/com/mosesmansaray/springboot/inegration/cucumber/SpringbootCucumberApplication.java
@@ -0,0 +1,12 @@
+package com.mosesmansaray.springboot.inegration.cucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCucumberApplication.class, args);
+	}
+}
